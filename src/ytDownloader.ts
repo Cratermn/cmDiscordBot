5a99f2c3a2b3543ba3863db58f703f65adbb74af
Crater <forgetcrater@gmail.com>
2025-10-10 15:29:11 -0700

    Updated node and changed the package used for wrapping yt-dlp

--- src/ytDownloader.ts
+++ src/ytDownloader.ts
@@ -1,19 +1,34 @@
 import { Downloader } from "./interfaces";
 import Song from "./song";
-
+import { YtDlp } from "ytdlp-nodejs";
 
 export default class YTDownloader extends Downloader {
-    private youtubedl = require("youtube-dl-exec");
+    private ytdlp: YtDlp;
+
     constructor() {
         super();
+        this.ytdlp = new YtDlp();
     }
 
-    public download(sng: Song): Promise<void> {
-        return this.youtubedl(sng.getURL(), {
-            extractAudio: true,
-            audioFormat: "mp3",
-            preferFfmpeg: true,
-            output: Downloader.mp3FilePath,
-        });
+    public async download(sng: Song): Promise<void> {
+        try {
+            await this.ytdlp.downloadAsync(sng.getURL(), {
+                format: {
+                    filter: "audioonly",
+                    quality: 1,
+                    type: "mp3"
+                },
+                output: Downloader.mp3FilePath,
+                onProgress: (progress) => {
+                    console.log(
+                        "Downloading..."
+                    );
+                }
+            });
+            console.log("Download completed successfully!");
+        } catch (error) {
+            console.error("YTDownloader error:", error);
+            throw error;
+        }
     }
 }
